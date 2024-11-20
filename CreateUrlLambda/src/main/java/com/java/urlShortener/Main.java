48cda4b667f4f5bd8edb4f463d2ece22e408100e
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-11-20 20:35:18 -0300

    refactor: use of factory to generate response

--- CreateUrlLambda/src/main/java/com/java/urlShortener/Main.java
+++ CreateUrlLambda/src/main/java/com/java/urlShortener/Main.java
@@ -2,37 +2,17 @@ package com.java.urlShortener;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.RuntimeJsonMappingException;
+import com.java.urlShortener.factory.ResponseFactory;
+import software.amazon.awssdk.services.s3.S3Client;
 
-import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> {
-    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    private final S3Client s3Client = S3Client.builder().build();
 
     @Override
     public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
-        String body = input.get("body").toString();
-
-        Map<String, String> bodyMap;
-
-        try {
-            bodyMap = objectMapper.readValue(body, Map.class);
-        } catch (JsonProcessingException exception) {
-            throw new RuntimeException("Error parsing JSON body: " + exception.getMessage(), exception);
-        }
-
-        String originalUrl = bodyMap.get("originalUrl");
-        String expirationTime = bodyMap.get("expirationTime");
-
-        String shortUrlCode = UUID.randomUUID().toString().substring(0,8);
-
-        Map<String,String> response = new HashMap<>();
-        response.put("code",shortUrlCode);
-
-        return response;
+        return ResponseFactory.createResponseFactory(input,context);
     }
 }
