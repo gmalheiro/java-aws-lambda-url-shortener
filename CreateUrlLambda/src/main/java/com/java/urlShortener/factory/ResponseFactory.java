5c7c8ae46901e68050316c1cdcf6c02bf91d4d0a
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-11-21 09:17:33 -0300

    feat: add S3 service to send data to S3

--- CreateUrlLambda/src/main/java/com/java/urlShortener/factory/ResponseFactory.java
+++ CreateUrlLambda/src/main/java/com/java/urlShortener/factory/ResponseFactory.java
@@ -2,6 +2,9 @@ package com.java.urlShortener.factory;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.java.urlShortener.entity.UrlData;
+import com.java.urlShortener.service.S3Service;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -9,8 +12,8 @@ import java.util.UUID;
 
 public class ResponseFactory {
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
-
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final S3Service s3Service = new S3Service();
     public static Map<String, String> createResponseFactory(Map<String, Object> input, Context context) {
         String body = input.get("body").toString();
 
@@ -27,6 +30,14 @@ public class ResponseFactory {
         long   expirationTimeInSeconds = Long.parseLong(expirationTime) * 3600;
 
         String shortUrlCode = UUID.randomUUID().toString().substring(0,8);
+
+        UrlData urlData = new UrlData(originalUrl,expirationTimeInSeconds);
+
+        try {
+            s3Service.insertIntoS3(urlData,shortUrlCode);
+        } catch(Exception e) {
+            throw new RuntimeException("An error happened: " + e.getMessage(), e);
+        }
 
         Map<String,String> response = new HashMap<>();
         response.put("code",shortUrlCode);
