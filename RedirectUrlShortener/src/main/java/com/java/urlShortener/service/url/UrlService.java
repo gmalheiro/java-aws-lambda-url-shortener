cd24cdc463d153c28eeffece81da2374a5b12450
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-11-23 23:22:25 -0300

    feat: add call to s3Service findObject method

--- RedirectUrlShortener/src/main/java/com/java/urlShortener/service/url/UrlService.java
+++ RedirectUrlShortener/src/main/java/com/java/urlShortener/service/url/UrlService.java
@@ -1,17 +1,20 @@
 package com.java.urlShortener.service.url;
 
+import com.java.urlShortener.service.s3.S3Service;
+import lombok.NoArgsConstructor;
+
 import java.util.Map;
 
+@NoArgsConstructor
 public class UrlService {
-    public Map<String,String> redirectUrl (Map<String, Object> input) {
+    private final S3Service s3Service = new S3Service();
+
+    public Map<String, Object> redirectUrl(Map<String, Object> input) {
         String pathParameters = (String) input.get("rawPath");
-        String shortUrlCode = pathParameters.replace( "/", "");
-
+        String shortUrlCode = pathParameters.replace("/", "");
         if (shortUrlCode == null) {
-            throw  new IllegalArgumentException("Ivalid input: 'shortUrlCode' is required");
+            throw new IllegalArgumentException("Ivalid input: 'shortUrlCode' is required");
         }
-
-
-        return  Map.of();
+        return s3Service.findObject(shortUrlCode);
     }
 }
