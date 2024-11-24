8b174521c4659ebbf5d301ad072121c5f867b137
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-11-23 23:22:03 -0300

    refactor: change lambda return type

--- RedirectUrlShortener/src/main/java/com/java/urlShortener/Main.java
+++ RedirectUrlShortener/src/main/java/com/java/urlShortener/Main.java
@@ -1,7 +1,17 @@
 package com.java.urlShortener;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.java.urlShortener.service.url.UrlService;
+
+import java.util.Map;
+
+public class Main implements RequestHandler<Map<String, Object>, Map<String, Object>> {
+
+    private final UrlService urlService = new UrlService();
+
+    @Override
+    public Map<String, Object> handleRequest(Map<String, Object> input, Context context) {
+        return urlService.redirectUrl(input);
     }
 }
